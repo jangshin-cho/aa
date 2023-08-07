ff577cdb6e17bae293c84a1ea44a1f72b8479ce0
jangshin-cho <83290851+jangshin-cho@users.noreply.github.com>
2023-08-07 15:24:11 +0900

    test
    ss

---
+++ src/Components/Middle/AddFilter.java
@@ -0,0 +1,19 @@
+package Components.Middle;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Framework.CommonFilterImpl;
+
+public class AddFilter extends CommonFilterImpl{
+
+	@Override
+	public boolean specificComputationForFilter() throws IOException {
+		List<String> updateLines = new ArrayList<>();
+		return false;
+		
+		
+        }
+	}
+
---
+++ src/Framework/CommonFilter.java
@@ -0,0 +1,15 @@
+package Framework;
+/**
+ * Copyright(c) 2019 All rights reserved by JU Consulting
+ */
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+public interface CommonFilter extends Runnable{
+    public void connectOutputTo(CommonFilter filter) throws IOException;
+    public void connectInputTo(CommonFilter filter) throws IOException;
+    public PipedInputStream getPipedInputStream();
+    public PipedOutputStream getPipedOutputStream();
+}
